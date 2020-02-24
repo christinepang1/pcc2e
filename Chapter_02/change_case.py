5f28409a9eb7156ee6b199fed38d693000ffa877
christinepang1 <christinezpang@gmail.com>
2020-02-24 04:04:40 +0000

    Ways to change a string name

---
+++ Chapter_02/change_case.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Changes case in numerous ways"""
+
+name = "ada lovelace"
+print("Original Name: {}".format(name))
+print("Name using title function: {}".format(name.title()))
+
+print("-----")
+print("Original Name: {}".format(name))
+print("Name using upper: {}".format(name.upper()))
+print("Name using lower: {}".format(name.lower()))
+#  using lower is helpful for storing data when you don't trust user's caps
